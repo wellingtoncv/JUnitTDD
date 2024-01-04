5fea236f1d330c50ce48398279a11a16abac01fa
Wellingtoncvs <wellington.santos@addasoftwares.com>
2024-01-04 17:22:49 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Testes automatizados no back end
+
+  Fundamentos de testes automatizados
+	  Tipos de testes
+	  Benefícios
+	  TDD - Test Driven Development
+	  Boas práticas e padrões
+
+JUnit
---
+++ src/tests/entities/AccountTests.java
@@ -0,0 +1,69 @@
+package tests.entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Account;
+import tests.factory.AccountFactory;
+
+public class AccountTests {
+
+	@Test
+	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+
+		double amount = 200.0;
+		double expectedValue = 196.0;
+		Account acc = AccountFactory.creatEmptyAccount();
+
+		acc.deposit(amount);
+		
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+		
+	}
+	
+	@Test
+	public void depositShouldDoNothingWhenNegativeAmout() {
+		
+		double expectedValue = 100.0;
+		Account acc = AccountFactory.createAccount(expectedValue);
+		double amount = -200.0;
+		
+		acc.deposit(amount);
+		
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+	
+	@Test
+	public void withdrawShouldDecreaseBalanceWhenSufficientBalance() {
+		
+		Account acc = AccountFactory.createAccount(800.0);
+		
+		acc.withdraw(500.0);
+		
+		Assertions.assertEquals(300.0, acc.getBalance());
+		
+	}
+	
+	@Test
+	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+		
+		double expectedValue = 0.0;
+		double initialBalance = 800.0;
+		Account acc = AccountFactory.createAccount(initialBalance);
+		
+		double result = acc.fullWithdraw();
+		
+		Assertions.assertTrue(expectedValue == acc.getBalance());
+		Assertions.assertTrue(result == initialBalance);
+	}
+	
+	@Test
+	public void withdrawShouldThrowExceptionsWhenInsufficientBalance() {
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Account acc = AccountFactory.createAccount(800.0);
+			acc.withdraw(801.0);
+		});
+	}
+
+}
